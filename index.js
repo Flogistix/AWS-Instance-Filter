612e032e8cd2957df7fe441af3c5b56900f9b055
Trevor Basinger <trevor.basinger@gmail.com>
2016-07-13 06:53:00 -0500

    Refactored and fixed problem where no tags existed

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 /* jshint laxcomma:true */
 const
   R = require ('ramda'),
+  Maybe = require ('data.maybe'),
+
+  toMaybe = function (x) {
+    if (R.isNil (x)) { return Maybe.Nothing (); }
+    return Maybe.Just (x);
+  },
+
+//maybesToList :: [Maybe a] -> [a]
+  maybesToList = function (xs) {
+    return xs.filter (R.prop ('isJust'))
+             .map    (function (m) { return m.get(); });
+  },
+
+//maybeProp :: String -> Maybe a
+  maybeProp = R.compose (toMaybe, R.prop),
 
   logI = function (x) { console.log (x); return x; },
 
@@ -11,19 +26,34 @@ const
   process.stdin.on ('data', function (d) { json += d.toString(); });
   process.stdin.on ('end',  function (d) {
     const
-      instances = JSON.parse (json),
+      instances       = JSON.parse (json),
       getReservations = R.prop ('Reservations'),
-      getInstances = R.prop ('Instances'),
-      isPriam = R.pipe ( R.map (R.prop ('Tags'))
-                       , R.map (R.compose (R.map (R.test (new RegExp(args[2],'gi'))), R.map (R.prop ('Value'))))
-                       , R.flatten
-                       , R.contains (true));
+      getInstances    = R.prop ('Instances'),
 
-    const getFilteredInstances = R.compose (R.flatten, R.filter (isPriam), R.map (getInstances), getReservations);
+    //getMatchingTags :: Tag -> [Maybe Bool]
+      getMatchingTags = R.pipe ( R.map (maybeProp ('Value'))                          // [Maybe String]
+                               , R.map (R.map (R.test (new RegExp(args[2], 'gi'))))), // [Maybe Bool]
+            
 
-    getIpAddressesOfPriams = R.compose (R.unnest, R.map (R.map (R.prop ('PrivateIpAddress'))), R.map (R.prop ('NetworkInterfaces')), getFilteredInstances);
 
-    console.log (JSON.stringify (getIpAddressesOfPriams (instances)));
+      isMatching = R.pipe ( R.map (maybeProp ('Tags'))      // [Maybe [Tag]]
+                          , R.map (R.map (getMatchingTags)) // [Maybe [Maybe Bool]]
+                          , R.map (R.map (maybesToList))    // [Maybe [Bool]]
+                          , maybesToList                    // [[Bool]]
+                          , R.flatten                       // [Bool]
+                          , R.contains (true)),
+
+      getFilteredInstances = R.pipe ( getReservations
+                                    , R.map    (getInstances)
+                                    , R.filter (isMatching)
+                                    , R.flatten ),
+
+      getIpAddressesOfMatching = R.pipe ( getFilteredInstances
+                                        , R.map (R.prop ('NetworkInterfaces'))
+                                        , R.map (R.map (R.prop ('PrivateIpAddress')))
+                                        , R.unnest );
+
+    console.log (JSON.stringify (getIpAddressesOfMatching (instances)));
   });
 })(process.argv);
 
